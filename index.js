d3867f4b619f5a7fba24f9fdee4bcbf1fdcee879
Olvin <olvin.bolanos@yahoo.com>
2020-04-20 18:35:10 -0700

    made the program run and collected the api and select a url

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 async function handleRequest(request) {
-  const NAME = 'experiment-0'
-  // Responses below are place holders, you could set up
-  // a custom path for each test (e.g. /control/somepath )
-  const TEST_RESPONSE = new Response('Test group') // fetch('/test/sompath', request)
-  const CONTROL_RESPONSE = new Response('Control group') // fetch('/control/sompath', request)
+  const getVariant = await fetch('https://cfw-takehome.developers.workers.dev/api/variants', {
+    headers: {
+      'Content-Type': 'application/json',
+      'method': 'GET',
+    },
+  })
+  
+  const init = {
+    headers: {
+      'Content-type': 'text/html;charset=UTF-8',
+    },
+  }
+ 
+  const parsedResponse = await getVariant.json();
+  
+  const NAME = 'refactoring'
+  
+  const TEST_RESPONSE = parsedResponse.variants[0] // fetch('/test/1', request)
+  const CONTROL_RESPONSE = parsedResponse.variants[1] // fetch('/control/2', request)
   // Determine which group this requester is in.
   const cookie = request.headers.get('cookie')
   if (cookie && cookie.includes(`${NAME}=control`)) {
-    return CONTROL_RESPONSE
+    return CONTROL_RESPONSE;
   } else if (cookie && cookie.includes(`${NAME}=test`)) {
     return TEST_RESPONSE
   } else {
@@ -15,14 +29,32 @@ async function handleRequest(request) {
     let group = Math.random() < 0.5 ? 'test' : 'control' // 50/50 split
     let response = group === 'control' ? CONTROL_RESPONSE : TEST_RESPONSE
     response.headers.append('Set-Cookie', `${NAME}=${group}; path=/`)
-    return response
+    const getURL = await fetch(response, init)
+    const results = await gatherResponse(getURL)
+    return new Response(results, init)
   }
 }
+
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
-
-
-
-
+/**
+   * gatherResponse awaits and returns a response body as a string.
+   * Use await gatherResponse(..) in an async function to get the response body
+   * @param {Response} response
+   */
+  
+async function gatherResponse(response) {
+  const { headers } = response
+  const contentType = headers.get('content-type')
+  if(contentType.includes('application/json')) {
+    return await response.json()
+  } else if (contentType.includes('application/text')) {
+    return await response.text()
+  } else if (contentType.includes('text/html')) {
+      return await response.text()
+  } else {
+    return await response.text()
+  } 
+}
 
